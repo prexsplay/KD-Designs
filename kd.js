2a1a55215a3d2392b5d873d2f5fad144da98200b
Gideon <giddieguypress@gmail.com>
2018-05-04 13:23:29 +0300

    Update kd.js

--- kd.js
+++ kd.js
@@ -1,1 +1,7 @@
+window.onscroll = function() {myFunction()};
 
+function myFunction() {
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+        document.getElementById("ptext").className = "slideUp";
+    }
+}
